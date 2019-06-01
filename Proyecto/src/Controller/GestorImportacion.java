a9eb73a7416c3d6465895bb003d8c0d5847bee85
jjomena <joaquinmena.84@gmail.com>
2019-06-01 11:03:16 -0600

    Actualizar Gestor de importacion

--- Proyecto/src/Controller/GestorImportacion.java
+++ Proyecto/src/Controller/GestorImportacion.java
@@ -1,5 +1,7 @@
  package Controller;
 
+import java.util.Date;
+
 /**
  *
  * @author Joaquin
@@ -10,11 +12,31 @@ public class GestorImportacion {
     
     public void LeerArchivo(String NombreArchivo){
         this.NombreArchivo = NombreArchivo;
+        /*Procesar el archivo*/
+        /*Enviar a LineaArchivo cada una de las líneas leídas*/
     }
     
-    public void LineaArchivo(int TareaPadre){
-        /*Aqui van todos los atributos*/
-        Ctrl.getDTOActividad().getUnaActividad().setTareaPadre(TareaPadre);
+    public void LineaArchivo(int IdActividadad,int idTarea, Date fechaCreacion,
+            Date fechaCompletado, Date fechaUltimaModificacion, String nombreTarea,
+            int idUsuario,String emailAsignado,Date fechaInicio,Date fechaFin,
+            String etiqueta, String nota,int idProyecto, int tareaPadre){
+        
+        Ctrl.getDTOActividad().getUnaActividad().setIdActividad(IdActividadad);
+        Ctrl.getDTOActividad().getUnaActividad().setIdTarea(idTarea);
+        Ctrl.getDTOActividad().getUnaActividad().setFechaCreacion(fechaCreacion);
+        Ctrl.getDTOActividad().getUnaActividad().setFechaCompletado(fechaCompletado);
+        Ctrl.getDTOActividad().getUnaActividad().setFechaUltimaModificacion(fechaUltimaModificacion);
+        Ctrl.getDTOActividad().getUnaActividad().setNombreTarea(nombreTarea);
+        Ctrl.getDTOActividad().getUnaActividad().setIdUsuario(idUsuario);
+        Ctrl.getDTOActividad().getUnaActividad().setEmailAsignado(emailAsignado);
+        Ctrl.getDTOActividad().getUnaActividad().setFechaInicio(fechaInicio);
+        Ctrl.getDTOActividad().getUnaActividad().setFechaFin(fechaFin);
+        Ctrl.getDTOActividad().getUnaActividad().setEtiqueta(etiqueta);
+        Ctrl.getDTOActividad().getUnaActividad().setNota(nota);
+        Ctrl.getDTOActividad().getUnaActividad().setIdProyecto(idProyecto);        
+        Ctrl.getDTOActividad().getUnaActividad().setTareaPadre(tareaPadre);
+        
+        Ctrl.CrearActividad();
         
     }    
 }
