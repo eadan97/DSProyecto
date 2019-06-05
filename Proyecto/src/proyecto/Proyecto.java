98c2ffea4d9957ec518a389bc17e76b240450779
jjomena <joaquinmena.84@gmail.com>
2019-06-05 14:13:54 -0600

    actualizcion DAoActividad

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -159,7 +159,7 @@ public class Proyecto {
     }
     
     
-    public static void mostrarActividadesUsuario(int idUsuario){
+    public static void mostrarActividadesUsuario(int idUsuario) throws SQLException{
         Ctrl.ConsultarActividades(idUsuario);
     }
     
