0a63769e3bd5257d07af2b966f3bc46b9b310f7e
ale <aletapiab28@gmail.com>
2019-06-05 10:57:41 -0600

    funcion act

---
+++ scripllenadoAvance.sql
@@ -0,0 +1,32 @@
+Insert into Avance (IdActividad , FechaAvance,HorasDedicadas,TipoAvance,IdUsuario, Descripción)
+VALUES (1,'2019-04-28',21,1,2,'Avance numero 1 usuario 2')
+
+Insert into Avance (IdActividad , FechaAvance,HorasDedicadas,TipoAvance,IdUsuario, Descripción)
+VALUES (2,'2019-05-28',21,1,3,'Avance numero 2 usuario 3')
+
+Insert into Avance (IdActividad , FechaAvance,HorasDedicadas,TipoAvance,IdUsuario, Descripción)
+VALUES (1,'2019-06-28',21,1,2,'Avance numero 3 usuario 2')
+
+Insert into Avance (IdActividad , FechaAvance,HorasDedicadas,TipoAvance,IdUsuario, Descripción)
+VALUES (11,'2019-06-28',214,1,3,'Avance numero 1 usuario 3')
+
+
+
+
+Insert INTO Evidencia (IdAvance)
+VALUES (1)
+
+Insert INTO Evidencia (IdAvance)
+VALUES (2)
+
+Insert INTO Evidencia (IdAvance)
+VALUES (3)
+
+Insert INTO Evidencia (IdAvance)
+VALUES (4)
+
+
+
+--SELECT * FROM Actividad
+SELECT * FROM Avance
+SELECT * FROM Evidencia
